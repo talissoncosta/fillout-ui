8e2968b1ba35bd1ee198345bf680bb56492da62f
Talisson Costa <talisson.odcosta@gmail.com>
2025-07-17 15:38:09 -0300

    [#3]: add buttons and documentation

---
+++ documentation/components/button/Button.stories.tsx
@@ -0,0 +1,63 @@
+import type { StoryObj } from '@storybook/react-vite';
+
+import { Button } from 'src/components/button';
+import {FileTextIcon} from "src/components/icons";
+import {colorIconActive, colorIconStandard} from "src/theme";
+import {styled} from "@linaria/react";
+import {useState} from "react";
+
+const meta = {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    isActive: {
+      control: 'boolean',
+      defaultValue: false,
+    },
+    onClick: { action: 'clicked' }
+  }
+}
+
+const ContentWrapper = styled('span')`
+  display: flex;
+  gap: 6px;
+`
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+export const Default: Story = {
+  args: {
+    children: 'Text example'
+  }
+};
+
+export const Active: Story = {
+  args: {
+    children: 'Text example',
+    variant: "active"
+  }
+};
+
+
+export const ToggleActiveState  = {
+  render: (args) => {
+    const [isActive, setIsActive] = useState(false)
+    const [isFocusVisible, setIsFocusVisible] = useState(false)
+    return (
+      <Button
+        {...args}
+        onKeyUp={() => setIsFocusVisible(true)}
+        onBlur={() => setIsFocusVisible(false)}
+        onClick={() => setIsActive(prev => !prev)}
+        variant={isActive ? "active" : "standard"}
+      >
+        <span className="flex">
+          <ContentWrapper>
+            <FileTextIcon color={isActive || isFocusVisible ? colorIconActive : colorIconStandard} />
+            Other
+          </ContentWrapper>
+        </span>
+      </Button>
+    )
+  }
+};
