875ee454c76814377a25235492b6022852874d94
Talisson Costa <talisson.odcosta@gmail.com>
2025-07-20 16:23:27 -0300

    [#36]: add custom logo and theme for storybook

---
+++ .storybook/fillout-theme.ts
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming/create';
+export default create({
+  base: 'light',
+  brandTitle: 'Fillout UI',
+  brandUrl: 'https://github.com/talissoncosta/fillout-ui',
+  brandImage: '../src/assets/fillout-logo-light.svg',
+});
